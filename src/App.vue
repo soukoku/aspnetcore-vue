669e0a02412a3d2b9ea7cd941a7f970167b7f8ea
Eugene Wang <yinchun.wang@outlook.com>
2018-06-29 21:24:13 -0400

    Working HMR except in IE/Edge.

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
-  </div>
+  </div> 
 </template>
 
 <style>
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+﻿module.exports = {
+    chainWebpack: (config) => {
+        // aspnet uses the other hmr so remove this one
+        // see https://github.com/webpack/webpack/issues/1583
+        config.plugins.delete('hmr')
+    }
+}
